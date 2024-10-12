3b95ec30871164e5ff75281f00d897e6c8be9c85
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-10-12 16:47:01 +0530

    reset password api

--- Backend/controllers/resetPassword.js
+++ Backend/controllers/resetPassword.js
@@ -2,6 +2,7 @@
 
 const User = require("../models/user");
 const mailSender = require("../utls/mailSender");
+const bcrypt = require("bcrypt");
 
 // reset password logic
 exports.resetPasswordToken = async (req, res) => {
@@ -42,6 +43,53 @@ exports.resetPasswordToken = async (req, res) => {
     res.status(200).json({ message: "password reset link sent" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal server while resting password" });
+    res
+      .status(500)
+      .json({ message: "Internal server while creating email (url)" });
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  try {
+    // data fetch
+    const { password, confirmPassword, token } = req.body;
+
+    // validation
+    if (password != confirmPassword) {
+      return res
+        .status(400)
+        .json({ message: "password and confirm password should be same" });
+    }
+    // get user details from db using token
+    const user = await User.findOne({ token });
+    // if not entry
+    if (!user) {
+      return res.status(404).json({ message: "token is not found or invalid" });
+    }
+
+    // token time check
+    if (user.resetPasswordExpires < Date.now()) {
+      return res.status(400).json({ message: "token is expired" });
+    }
+    // hash password
+    const hashedPassword = await bcrypt(password, 10);
+
+    // password update
+    await User.findOneAndUpdate(
+      { token: token },
+      { password: hashedPassword },
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json({
+      message: "password updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Internal server while resetting password" });
   }
 };
